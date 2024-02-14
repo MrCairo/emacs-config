cecd5507c070a2cbef581f28c3f4dd728598fa16
Mitch Fisher <mitch@committed-code.com>
2024-02-14 09:59:01 -0500

    Updates to custom and early-init el files.

---
+++ custom-vars.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(ef-symbiosis))
+ '(custom-safe-themes
+     '("47023ac7cf83cbdd006634896d9689262f3b746c4578802ee808202aaf585c04" "091fd0cf919886a67fde0b4dd02a9379cabc7f777ab604b02a874137e24ec7fd" default))
+ '(warning-suppress-log-types
+     '(((python python-shell-completion-native-turn-on-maybe))
+	 ((package reinitialization))
+	 (comp)
+	 (treesit)
+	 (use-package)
+	 (python-mode)
+	 (package-initialize))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t :background "#131c2b")))
+ '(header-line ((t :box (:line-width 4 :color "#1d2c39" :style nil))))
+ '(header-line-highlight ((t :box (:color "#eaedef"))))
+ '(keycast-key ((t)))
+ '(line-number ((t :background "#131c2b")))
+ '(mode-line ((t :box (:line-width 6 :color "#165f70" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#165f70" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#dfd0d5"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#352f2f" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#15050f" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#554f4f" :style nil))))
+ '(window-divider ((t :background "#15050f" :foreground "#15050f")))
+ '(window-divider-first-pixel ((t :background "#15050f" :foreground "#15050f")))
+ '(window-divider-last-pixel ((t :background "#15050f" :foreground "#15050f"))))
--- early-init.el
+++ early-init.el
@@ -1,4 +1,46 @@
 ;; Early initialization file for emacs
 
+;; Uncomment when using 'straight' package
 (setq package-enable-at-startup nil)
 
+;;; =========================================================================
+;;; startup
+;;; The default is 800 kilobytes.  Measured in bytes.
+;;; Process performance tuning
+;;
+;; ** See early-init.el where some of the allocation variables are set **
+;;
+
+(add-hook 'before-init-hook
+   (lambda ()
+      ;; warn when opening files bigger than 100MB
+      (setq large-file-warning-threshold 100000000)
+      ;; reduce the frequency of garbage collection by making it happen on
+      ;; each 50MB of allocated data (the default is on every 0.76MB)
+      (setq gc-cons-threshold 50000000)))
+
+(setq read-process-output-max (* 4 1024 1024))
+(setq process-adaptive-read-buffering nil)
+
+;;; -------------------------------------------------------------------------
+;;; Package setup
+
+(require 'package)
+(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '( "melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '( "org" . "https://orgmode.org/elpa/") t)
+
+(package-initialize)
+
+(unless package-archive-contents
+   (package-refresh-contents))
+
+(defun mrf/display-startup-time ()
+   "Calculate and display startup time."
+   (message "Emacs loaded in %s with %d garbage collections."
+      (format "%.2f seconds"
+         (float-time
+  	  (time-subtract after-init-time before-init-time)))
+  	gcs-done))
+
+(add-hook 'emacs-startup-hook #'mrf/display-startup-time)
