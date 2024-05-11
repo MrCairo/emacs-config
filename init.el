30b6b20eb00caf8952d44cbddb02f1e963f16fb7
Mitch Fisher <mitch@committed-code.com>
2024-05-11 13:06:35 -0400

    Updates

--- init.el
+++ init.el
@@ -1932,18 +1932,16 @@ Thanks @wyuenho on GitHub"
 
 (defun define-dape-hydra ()
    (defhydra dape-hydra (:color pink :hint nil :foreign-keys run)
-	"
-  ^Stepping^		^Switch^		 ^Breakpoints^		^Debug^			    ^Eval
+      "
+  ^Stepping^          ^Switch^                 ^Breakpoints^          ^Debug^                     ^Eval
   ^^^^^^^^----------------------------------------------------------------------------------------------------------------
-  _._: Next		_st_: Thread		_bb_: Toggle	       _dd_: Debug		   _ee_: Eval Expression
-  _/_: Step in	_si_: Info		_bd_: Delete	       _dw_: Watch dwim
-  _,_: Step out	_sf_: Stack Frame	_ba_: Add	       _dx_: end session
-  _c_: Continue	_su_: Up stack frame	_bc_: Set condition    _dX_: end all sessions
-  _r_: Restart frame	_sd_: Down stack frame	_bl_: Set log message
-  _Q_: Disconnect	_sR_: Session Repl
-			_sU_: Info Update
-
-"
+  _._: Next           _st_: Thread             _bb_: Toggle           _dd_: Debug                 _ee_: Eval Expression
+  _/_: Step in        _si_: Info               _bd_: Delete           _dw_: Watch dwim
+  _,_: Step out       _sf_: Stack Frame        _ba_: Add              _dx_: end session
+  _c_: Continue       _su_: Up stack frame     _bc_: Set condition    _dX_: end all sessions
+  _r_: Restart frame  _sd_: Down stack frame   _bl_: Set log message
+  _Q_: Disconnect     _sR_: Session Repl
+                    _sU_: Info Update"
 	("n" dape-next)
 	("i" dape-step-in)
 	("o" dape-step-out)
@@ -3002,7 +3000,6 @@ Thanks @wyuenho on GitHub"
 	      ("M-RET f" . mrf/set-fill-column-interactively)
 	      ("M-RET i" . ielm)
 	      ("M-RET v" . vterm-other-window)
-	      ("M-RET S" . smartparens-strict-mode)
 	      ("M-RET t" . treemacs)
 	      ("M-RET |" . global-display-fill-column-indicator-mode)
 	      ("M-RET T +" . next-theme)
