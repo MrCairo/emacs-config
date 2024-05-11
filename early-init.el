43d8aab8907bebe2d37498f8ed3c0b102f1b0767
Mitch Fisher <mitch@committed-code.com>
2024-05-11 11:04:00 -0400

    Tab size change to 3 instead of 4 to clean up clutter.
    Addressed partly a problem with DAPE not being loaded when
    "C-c ." is first pressed. Still some work there.
    
    Signed-off-by: Mitch Fisher <mitch@committed-code.com>

--- early-init.el
+++ early-init.el
@@ -24,21 +24,21 @@
 ;; be changed (as it does in my Config.org). Doing this will prevent Emacs
 ;; from compiling all the .eln files upon startup.
 (when (boundp 'native-comp-eln-load-path)
-    (startup-redirect-eln-cache (expand-file-name "eln-cache/" user-emacs-directory)))
+   (startup-redirect-eln-cache (expand-file-name "eln-cache/" user-emacs-directory)))
 
 (add-hook 'before-init-hook
-    (lambda ()
-        (setq read-process-output-max (* 64 1024 1024)) ;; 1mb
-        (setq process-adaptive-read-buffering nil)
-        ;; warn when opening files bigger than 100MB
-        (setq large-file-warning-threshold 100000000)
-        ;; reduce the frequency of garbage collection by making it happen on
-        ;; each 50MB of allocated data (the default is on every 0.76MB)
-        ;; Garbage Collections
-        (setq gc-cons-percentage 0.5)
-        (setq gc-cons-threshold 50000000)))
+   (lambda ()
+	(setq read-process-output-max (* 64 1024 1024)) ;; 1mb
+	(setq process-adaptive-read-buffering nil)
+	;; warn when opening files bigger than 100MB
+	(setq large-file-warning-threshold 100000000)
+	;; reduce the frequency of garbage collection by making it happen on
+	;; each 50MB of allocated data (the default is on every 0.76MB)
+	;; Garbage Collections
+	;; (setq gc-cons-percentage 0.5)
+	(setq gc-cons-threshold 50000000)))
 
-(setq read-process-output-max (* 80 1024 1024))
+(setq read-process-output-max (* 16 1024 1024))
 (setq process-adaptive-read-buffering nil)
 ;; (add-to-list 'default-frame-alist '(undecorated . t))
 
@@ -48,15 +48,15 @@
 (add-hook 'package-menu-mode-hook #'hl-line-mode)
 
 (setq package-archives
-    '(( "gnu-elpa" . "https://elpa.gnu.org/packages/")
-         ( "nongnu" . "https://elpa.nongnu.org/nongnu/")
-         ( "melpa" . "https://melpa.org/packages/")
-         ( "org" . "https://orgmode.org/elpa/")
-         ( "melpa-stable" . "https://stable.melpa.org/packages/")))
+   '(( "gnu-elpa" . "https://elpa.gnu.org/packages/")
+	 ( "nongnu" . "https://elpa.nongnu.org/nongnu/")
+	 ( "melpa" . "https://melpa.org/packages/")
+	 ( "org" . "https://orgmode.org/elpa/")
+	 ( "melpa-stable" . "https://stable.melpa.org/packages/")))
 
 ;; Highest number gets priority (what is not mentioned has priority 0)
 (setq package-archive-priorities
-    '(
+   '(
 	 ( "org" . 99 )
 	 ( "melpa" . 40 )
 	 ( "gnu-elpa" . 30 )
@@ -67,45 +67,45 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; w/o this Emacs freezes when refreshing ELPA
 
 (defun add-site-lisp-to-load-path (parent-dir)
-    "Add every non-hidden subdir of PARENT-DIR to `load-path'."
-    (use-package cl-lib)
-    (let ((default-directory parent-dir))
-  	(setq load-path
-            (append
-  		(cl-remove-if-not
-  		    #'file-directory-p
-  		    (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
-  		load-path))))
+   "Add every non-hidden subdir of PARENT-DIR to `load-path'."
+   (use-package cl-lib)
+   (let ((default-directory parent-dir))
+	(setq load-path
+	   (append
+	      (cl-remove-if-not
+		 #'file-directory-p
+		 (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
+	      load-path))))
 
 ;; Add both site-lisp and its immediate subdirs to `load-path'
 (when (file-directory-p (expand-file-name "site-lisp/" user-emacs-directory))
-    (let ((site-lisp-dir (expand-file-name "site-lisp/" user-emacs-directory)))
-  	(push site-lisp-dir load-path)
-  	(add-site-lisp-to-load-path site-lisp-dir)))
+   (let ((site-lisp-dir (expand-file-name "site-lisp/" user-emacs-directory)))
+	(push site-lisp-dir load-path)
+	(add-site-lisp-to-load-path site-lisp-dir)))
 
 (setq use-package-compute-statistics t
-    use-package-verbose t
-    use-package-always-ensure nil
-    use-package-always-demand nil
-    use-package-always-defer nil)
-
-(use-package gcmh
-    :diminish gcmh-mode
-    :config
-    (setq gcmh-idle-delay 5
-        gcmh-high-cons-threshold (* 16 1024 1024))  ; 16mb
-    (gcmh-mode 1))
+   use-package-verbose t
+   use-package-always-ensure nil
+   use-package-always-demand nil
+   use-package-always-defer nil)
 
 (add-hook 'emacs-startup-hook
-    (lambda ()
-        (setq gc-cons-percentage 0.1))) ;; Default value for `gc-cons-percentage'
+   (lambda ()
+	(setq startup-time-message
+	   (format "Emacs read in %.2f seconds with %d garbage collections."
+	      (float-time (time-subtract after-init-time before-init-time))
+	      gcs-done))
+	(message startup-time-message)))
 
-(add-hook 'emacs-startup-hook
-    (lambda ()
-        (message "Emacs ready in %s with %d garbage collections."
-            (format "%.2f seconds"
-                (float-time
-                    (time-subtract after-init-time before-init-time)))
-            gcs-done)))
+;; (use-package gcmh
+;;	 :diminish gcmh-mode
+;;	 :config
+;;	 (setq gcmh-idle-delay 5
+;;	     gcmh-high-cons-threshold (* 16 1024 1024))	 ; 16mb
+;;	 (gcmh-mode 1))
+
+;; (add-hook 'emacs-startup-hook
+;;	 (lambda ()
+;;	     (setq gc-cons-percentage 0.1))) ;; Default value for `gc-cons-percentage'
 
 ;;; early-init.el ends here.
