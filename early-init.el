bde64b310f94ca5d931266c45660953114e8c4a1
Mitch Fisher <mitch@committed-code.com>
2024-05-13 08:29:52 -0400

    missed updates

--- early-init.el
+++ early-init.el
@@ -12,21 +12,21 @@
 (add-hook 'package-menu-mode-hook #'hl-line-mode)
 
 (setq package-archives
-   '(( "gnu-elpa" . "https://elpa.gnu.org/packages/")
-       ( "nongnu" . "https://elpa.nongnu.org/nongnu/")
-       ( "melpa" . "https://melpa.org/packages/")
-       ( "org" . "https://orgmode.org/elpa/")
-       ( "melpa-stable" . "https://stable.melpa.org/packages/")))
+  '(( "gnu-elpa" . "https://elpa.gnu.org/packages/")
+     ( "nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ( "melpa" . "https://melpa.org/packages/")
+     ( "org" . "https://orgmode.org/elpa/")
+     ( "melpa-stable" . "https://stable.melpa.org/packages/")))
 
 ;; Highest number gets priority (what is not mentioned has priority 0)
 (setq package-archive-priorities
-   '(
-       ( "org" . 99 )
-       ( "melpa" . 40 )
-       ( "gnu-elpa" . 30 )
-       ( "melpa-stable" . 20 )
-       ( "nongnu" . 10)
-       ))
+  '(
+     ( "org" . 99 )
+     ( "melpa" . 40 )
+     ( "gnu-elpa" . 30 )
+     ( "melpa-stable" . 20 )
+     ( "nongnu" . 10)
+     ))
 
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; w/o this Emacs freezes when refreshing ELPA
 
@@ -48,28 +48,28 @@
 ;; 	(add-site-lisp-to-load-path site-lisp-dir)))
 
 (setq use-package-compute-statistics t
-   use-package-verbose t
-   use-package-always-ensure nil
-   use-package-always-demand nil
-   use-package-always-defer nil)
+  use-package-verbose t
+  use-package-always-ensure nil
+  use-package-always-demand nil
+  use-package-always-defer nil)
 
 (add-hook 'emacs-startup-hook
-   (lambda ()
-      (setq startup-time-message
-	 (format "Emacs read in %.2f seconds with %d garbage collections."
-	    (float-time (time-subtract after-init-time before-init-time))
-	    gcs-done))
-      (message startup-time-message)))
+  (lambda ()
+    (setq startup-time-message
+      (format "Emacs read in %.2f seconds with %d garbage collections."
+      (float-time (time-subtract after-init-time before-init-time))
+      gcs-done))
+    (message startup-time-message)))
 
 (use-package gcmh
-       :diminish gcmh-mode
-       :config
-       (setq gcmh-idle-delay 5
-	   gcmh-high-cons-threshold (* 16 1024 1024))	 ; 16mb
-       (gcmh-mode 1))
+  :diminish gcmh-mode
+  :config
+  (setq gcmh-idle-delay 5
+    gcmh-high-cons-threshold (* 16 1024 1024))	 ; 16mb
+  (gcmh-mode 1))
 
 (add-hook 'emacs-startup-hook
-       (lambda ()
-	   (setq gc-cons-percentage 0.1))) ;; Default value for `gc-cons-percentage'
+  (lambda ()
+    (setq gc-cons-percentage 0.1))) ;; Default value for `gc-cons-percentage'
 
 ;;; early-init.el ends here.
