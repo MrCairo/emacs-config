5c3371fd4634c63b172020d8f94ba310a3cb2ffd
Mitch Fisher <mitch@committed-code.com>
2024-07-28 15:02:09 -0400

    Add in lisp/opam-std-libs.el which is used as a placeholder for use-package so that it can perform a system install if OCaml libs are missing.

---
+++ lisp/opam-std-libs.el
@@ -0,0 +1,12 @@
+;;; opam-std-libs --- Stub installer for use-package
+
+;;; Commentary:
+;; This module is meant to be used by 'use-package' with the
+;; :ensure-system-package attribute for the OCaml standard libraries.
+;;
+
+;;; Code:
+
+(provide 'opam-std-libs)
+
+;;; opam-emacs-setup.el ends here
